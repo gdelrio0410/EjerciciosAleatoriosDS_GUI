0b86bc5b78a0796d60f9e638bab595409f8d73a5
cprieto76 <115907710+cprieto76@users.noreply.github.com>
2023-01-27 10:35:16 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from tkinter import *
-import random
+from tkinter import * # Libreria para crear interfaces graficas
+import random   # modulo para generar numeros aleatorios o escoger elementos de una lista aleatoriamente
 
 diccionario_preguntas = {"uno": "pregunta 1", "dos": "pregunta 2", "tres": "pregunta 3", "cuatro": "pregunta 4",
                          "cinco": "pregunta 5"}
 lista_preguntas = []
-raiz = Tk()
+raiz = Tk()  # Esta funcionn del modulo tkinter crea una aplicacion tkinter. Crea una ventana nivel alto (top-level). La funcion retorna un objeto
+              #, asignado tipicamente a una variable
 raiz.title("preguntas")
 
 
